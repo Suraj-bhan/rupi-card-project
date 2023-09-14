6a7460ab18a09384b41d4f28633127d09d88a892
Suraj-bhan <surajbhanmundotiya@gmail.com>
2023-09-14 23:56:44 +0530

    Fixed small justifcation

--- components/downloadsection.tsx
+++ components/downloadsection.tsx
@@ -4,7 +4,7 @@ const DownloadSection = () => {
       <div className="md:flex-[50%] text-2xl md:text-[42px] leading-normal  font-medium">
         Download now to get started
       </div>
-      <div className="flex md:flex-[50%]">
+      <div className="flex md:flex-[50%] justify-end">
         <button className="w-32 md:w-56 h-10 md:h-[60px] leading-8 md:leading-[3rem] text-center text-[14px] md:text-lg mr-[10px] border-2 rounded-xl border-black">
           Google Play
         </button>
